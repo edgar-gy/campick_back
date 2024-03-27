8a2f7e451860d0b524f891300bd7880d5712910f
jjoonyna <nwj3808@gmail.com>
2024-03-27 10:40:19 +0900

    기본 환경 설정 완료

---
+++ src/main/java/com/choongang/campick/CampickApplication.java
@@ -0,0 +1,13 @@
+package com.choongang.campick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampickApplication.class, args);
+	}
+
+}
